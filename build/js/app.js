ef6e46df013a25aa250d36cca2977f41b09b9d23
Laura López <99351746+laurity@users.noreply.github.com>
2024-02-21 10:38:17 +0100

    templates

--- build/js/app.js
+++ build/js/app.js
@@ -1,91 +1,109 @@
-document.addEventListener("DOMContentLoaded", function(){
-    iniciarApp()
-})
+document.addEventListener('DOMContentLoaded', function(){
+    iniciarApp();
+});
 
-const iniciarApp = () =>{
-    mostrarAnio()
-    mostrarImagen()
-    ScrollBar()
-    mostrarModal()
-    darkMode()
-}
-
-const ScrollBar = () => {
+function iniciarApp() {
     
+    navegacionFija();
 
+    crearGaleria();
+
+    scrollNav();
+
+    darkMode();
+
+     let yearSpan = document.getElementById("year");
+
+     let currentYear = new Date().getFullYear();
+
+     yearSpan.textContent = currentYear;
 }
 
-const crearImagenes = () =>{
-    const galeria = document.querySelector(".galeria-imagenes")
-    for(let i = 1; i <= 5; i++){
-        const imagen = document.createElement("picture")
-        imagen.src = `build/img/thumb/${i}.webp`
-        imagen.onclick = mostrarImagen
 
-        const lista = document.createElement("LI")
-        lista.appendChild(imagen)
-        galeria.appendChild(lista)
+function navegacionFija() {
+    const barra = document.querySelector('.header');
+    const video = document.querySelector('.video');
+    const body = document.querySelector('body');
+
+
+    window.addEventListener('scroll', function() {
+        if( video.getBoundingClientRect().bottom < 0 && this.window.innerWidth >= 768 ) {
+            barra.classList.add('fijo');
+            body.classList.add('body-scroll');
+        } else {
+            barra.classList.remove('fijo');
+            body.classList.remove('body-scroll');
+        }
+    });
+}
+
+
+function scrollNav() {
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click', ({ target }) => {
+            event.preventDefault();
+            const seccionScroll = target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({ behavior: "smooth" });
+        });
+    });
+}
+
+function crearGaleria() {
+    const galeria = document.querySelector('.galeria-imagenes');
+    for(let i = 1; i <= 5; i++ ) {
+        const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+            <source srcset="build/img/thumb/${i}.png" type="image/png">
+            <source srcset="build/img/thumb/${i}.jpg" type="image/jpg">
+            <source srcset="build/img/thumb/${i}.webp" type="image/webp">
+            <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.png" alt="imagen galeria">
+        `;
+        imagen.onclick = function() {
+            mostrarImagen(i);
+        }
+        galeria.appendChild(imagen);
     }
+}  
 
-}
+function mostrarImagen(id) {
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+        <source srcset="build/img/grande/${id}.png" type="image/png">
+        <source srcset="build/img/grande/${id}.jpg" type="image/jpg">
+        <source srcset="build/img/grande/${id}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.png" alt="imagen galeria">
+    `;
 
-const mostrarImagen = () => {
-    const id = parseInt(imagen.dataset.imagenId)
-    const imagen = document.createElement("img")
-    imagen.src = `build/img/grande/${id}.png`
-    imagen.src = `build/img/grande/${id}.jpg`
-    imagen.src = `build/img/grande/${id}.webp`
-
-    const overlay = document.createElement("div")
-    overlay.appendChild(imagen)
-    overlay.classList.add("new-overlay")
-
-    overlay.onclick = () => {
-        overlay.remove()
+    const overlay = document.createElement('div');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+    overlay.onclick = function() {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
     }
-    const cerrarImagen = document.createElement("img")
-    cerrarImagen.textContent = "X"
-    cerrarImagen.classList.add("btn-cerrar")
-
-    cerrarImagen.onclick = () => {
-        overlay.remove()
+    const cerrarModal = document.createElement('P');
+    cerrarModal.textContent = 'X';
+    cerrarModal.classList.add('btn-cerrar');
+    cerrarModal.onclick = function() {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
     }
+    overlay.appendChild(cerrarModal);
 
-    overlay.appendChild(cerrarImagen)
-    const body = document.querySelector("body")
-    body.appendChild(overlay)
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    body.classList.add('fijar-body');
 }
 
-const crearModal = () => {
-    const modal = document.createElement("div")
-    modal.classList.add(modal)
-    modal.innerHTML = `
-
-    `
-}
-
-const mostrarModal = () =>{
-    const imagenes = document.querySelectorAll(".galeria-imagenes img")
-    imagenes.forEach(imagen => {
-        imagen.addEventListener("click", () => {
-            console.log("click")
-        })
-    })
-}
-
-const darkMode = () => {
-    const darkMode = document.getElementById("dark-mode")
-
-    darkMode.addEventListener("click", () => {
-        document.body.classList.toggle("dark")
-    })
-
-    body.style.backgroundColor = "black"
-    body.style.color = "white"
-}
-
-const mostrarAnio = () => {
-    const year = document.getElementById("year")
-    year = getFullYear().textContent;
-    
+function darkMode() {
+    const botonDarkMode = document.querySelector('#dark-mode-button');
+    const body = document.querySelector('body');
+    botonDarkMode.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+    });
 }
