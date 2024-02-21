91db7f888f719f80f04dd29101ca4db0cd5b44f8
Laura López <99351746+laurity@users.noreply.github.com>
2024-02-21 10:06:50 +0100

    mostrado de imagenes

--- build/js/app.js
+++ build/js/app.js
@@ -3,11 +3,27 @@ document.addEventListener("DOMContentLoaded", function(){
 })
 
 const iniciarApp = () =>{
-
+    mostrarImagenes();
 }
 
 const ScrollBar = () => {
 
+}
+
+const mostrarImagenes = () => {
+    const lista = document.querySelector("galeria-imagenes")
+
+    for (let i = 1; i <= 5; i++) {
+    const imagen = document.createElement("img")
+    imagen.innerHTML = `
+        <source src="build/img/thumb/${i}.png" alt="imagen ${i}">
+        <source src="build/img/thumb/${i}.jpg" alt="imagen ${i}">
+        <source src="build/img/thumb/${i}.webp" alt="imagen ${i}">
+
+        <img src="build/img/thumb/${i}.png" alt="imagen ${i}">
+    `
+        
+    }
 }
 
 const crearModal = () => {
