d54da8e09a4ae203054c09a4cee96d27e81909a4
Laura López <99351746+laurity@users.noreply.github.com>
2024-02-21 10:17:21 +0100

    implementadas mas cosinas

--- build/js/app.js
+++ build/js/app.js
@@ -3,42 +3,72 @@ document.addEventListener("DOMContentLoaded", function(){
 })
 
 const iniciarApp = () =>{
-    mostrarImagenes();
+    mostrarImagen();
     ScrollBar()
     mostrarModal()
 }
 
 const ScrollBar = () => {
+    const barra = document.querySelectorAll("contenedor-header")
+    barra.addEventListener("scroll", () => {
+        
+    })
 
 }
 
-const mostrarImagenes = () => {
-    const lista = document.querySelector("galeria-imagenes")
+const crearImagenes = () =>{
+    const galeria = document.querySelector(".galeria-imagenes")
+    for(let i = 1; i <= 5; i++){
+        const imagen = document.createElement("picture")
+        imagen.src = `build/img/thumb/${i}.webp`
+        imagen.onclick = mostrarImagen
 
-    for (let i = 1; i <= 5; i++) {
-    const imagen = document.createElement("img")
-    imagen.innerHTML = `
-        <source src="build/img/thumb/${i}.png" alt="imagen ${i}">
-        <source src="build/img/thumb/${i}.jpg" alt="imagen ${i}">
-        <source src="build/img/thumb/${i}.webp" alt="imagen ${i}">
-
-        <img src="build/img/thumb/${i}.png" alt="imagen ${i}">
-    `
-        
+        const lista = document.createElement("LI")
+        lista.appendChild(imagen)
+        galeria.appendChild(lista)
     }
+
+}
+
+const mostrarImagen = () => {
+    const id = parseInt(imagen.dataset.imagenId)
+    const imagen = document.createElement("img")
+    imagen.src = `build/img/grande/${id}.webp`
+
+    const overlay = document.createElement("div")
+    overlay.appendChild(imagen)
+    overlay.classList.add("overlay")
+
+    overlay.onclick = () => {
+        overlay.remove()
+    }
+    const cerrarImagen = document.createElement("p")
+    cerrarImagen.textContent = "X"
+    cerrarImagen.classList.add("btn-cerrar")
+
+    cerrarImagen.onclick = () => {
+        overlay.remove()
+    }
+
+    overlay.appendChild(cerrarImagen)
+    const body = document.querySelector("body")
+    body.appendChild(overlay)
 }
 
 const crearModal = () => {
-    //creamos el modal
     const modal = document.createElement("div")
     modal.classList.add(modal)
     modal.innerHTML = `
-
     `
 }
 
 const mostrarModal = () =>{
-
+    const imagenes = document.querySelectorAll(".galeria-imagenes img")
+    imagenes.forEach(imagen => {
+        imagen.addEventListener("click", () => {
+            console.log("click")
+        })
+    })
 }
 
 const darkMode = () => {
